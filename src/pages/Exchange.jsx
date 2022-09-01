ca7424924bace5ef50493b7fd05b1f9ba8477fc8
lbianlbian <83378530+lbianlbian@users.noreply.github.com>
2022-08-31 21:49:10 -0700

    Update Exchange.jsx

--- src/pages/Exchange.jsx
+++ src/pages/Exchange.jsx
@@ -22,7 +22,7 @@ const Exchange = () => {
   const [catId, setCatId] = useState(0);
   const avaiBets = async () => {
     try {
-      const res = await axios.get("https://script.google.com/macros/s/AKfycbxCY_YhtNe-GCBm98zDC11eSx6p9-cwQSfxS56BM3dCpY6cl5EbM-RU5zSn2_x3ex4HQg/exec");
+      const res = await axios.get("https://script.google.com/macros/s/AKfycbxzvjIzPgO0A0rUYD1FHHD-lv5zRzXLVZLwOXiLMBCPAuHMbmHnsG5FFgn_E9yA4xj3UA/exec");
       console.log(res.data);
       setAvaiOdds(res.data);
     } catch (error) {
